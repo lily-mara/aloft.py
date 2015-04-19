b57b298618f5b5a469ef35fa42278cb318c86bcf
Nate Mara <natemara@gmail.com>
2015-04-19 18:29:30 -0400

    initial commit

---
+++ aloft/tests/__main__.py
@@ -0,0 +1,86 @@
+from unittest import TestCase, main
+import os
+
+from aloft.api import (_find_station_line, _parse_station_line, winds_aloft,
+	_find_data_block)
+
+BASE_DIR = os.path.dirname(__file__)
+
+
+class TestParseStationLine(TestCase):
+	def setUp(self):
+		with open(os.path.join(BASE_DIR, 'example_data.txt'), 'r') as data_file:
+			self.data = data_file.readlines()
+		self.line = _find_station_line('CVG', self.data)
+		self.winds = _parse_station_line(self.line)
+
+	def test_6k_winds(self):
+		winds = self.winds.winds[6000]
+		self.assertEqual(winds.direction, 220)
+		self.assertEqual(winds.speed, 36)
+
+	def test_3k_winds(self):
+		winds = self.winds.winds[3000]
+		self.assertEqual(winds.direction, 190)
+		self.assertEqual(winds.speed, 30)
+
+	def test_9k_winds(self):
+		winds = self.winds.winds[9000]
+		self.assertEqual(winds.direction, 210)
+		self.assertEqual(winds.speed, 35)
+
+
+class TestParseStationLineSomeNone(TestCase):
+	def setUp(self):
+		with open(os.path.join(BASE_DIR, 'example_data.txt'), 'r') as data_file:
+			self.data = data_file.readlines()
+		self.line = _find_station_line('DEN', self.data)
+		self.winds = _parse_station_line(self.line)
+
+	def test_6k_winds(self):
+		winds = self.winds.winds[6000]
+		self.assertIsNone(winds)
+
+	def test_3k_winds(self):
+		winds = self.winds.winds[3000]
+		self.assertIsNone(winds)
+
+	def test_9k_winds(self):
+		winds = self.winds.winds[9000]
+		self.assertEqual(winds.direction, 360)
+		self.assertEqual(winds.speed, 8)
+
+
+class TestFindStationLine(TestCase):
+	def setUp(self):
+		with open(os.path.join(BASE_DIR, 'example_data.txt'), 'r') as data_file:
+			self.data = data_file.readlines()
+
+	def test_cvg_lowercase_airport_code(self):
+		station_line = _find_station_line('cvg', self.data)
+		self.assertIn('CVG', station_line)
+
+	def test_cvg_uppercase_airport_code(self):
+		station_line = _find_station_line('CVG', self.data)
+		self.assertIn('CVG', station_line)
+
+	def test_cvg_leading_spaces_airport_code(self):
+		station_line = _find_station_line('      CVG', self.data)
+		self.assertIn('CVG', station_line)
+
+	def test_cvg_trailing_spaces_airport_code(self):
+		station_line = _find_station_line('CVG      ', self.data)
+		self.assertIn('CVG', station_line)
+
+	def test_cvg_leading_and_trailing_spaces_airport_code(self):
+		station_line = _find_station_line('   CVG   ', self.data)
+		self.assertIn('CVG', station_line)
+
+	def test_bogus_airport_code(self):
+		with self.assertRaises(ValueError):
+			_find_station_line('fjaksdlf', self.data)
+
+
+
+if __name__ == '__main__':
+	main()
