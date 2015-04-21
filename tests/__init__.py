601e05b979b2d0acaed3aa97035899f03c24a50b
Nate Mara <natemara@gmail.com>
2015-04-20 23:03:44 -0400

    add tests for dict method

--- tests/__init__.py
+++ tests/__init__.py
@@ -6,6 +6,49 @@ from aloft import _find_station_line, _parse_station_line
 BASE_DIR = os.path.dirname(__file__)
 
 
+class TestWindsAloftDictAllGood(TestCase):
+	def setUp(self):
+		with open(os.path.join(BASE_DIR, 'example_data.txt'), 'r') as data_file:
+			self.data = data_file.readlines()
+		self.line = _find_station_line('CVG', self.data)
+		self.winds = _parse_station_line(self.line)
+		self.winds_dict = self.winds.dict()
+
+	def test_3k_winds(self):
+		self.assertEqual(self.winds_dict['winds'][3000]['direction'], 190)
+		self.assertEqual(self.winds_dict['winds'][3000]['speed'], 30)
+
+	def test_6k_winds(self):
+		self.assertEqual(self.winds_dict['winds'][6000]['direction'], 220)
+		self.assertEqual(self.winds_dict['winds'][6000]['speed'], 36)
+
+	def test_9k_winds(self):
+		self.assertEqual(self.winds_dict['winds'][9000]['direction'], 210)
+		self.assertEqual(self.winds_dict['winds'][9000]['speed'], 35)
+
+
+class TestWindsAloftDictSomeNone(TestCase):
+	def setUp(self):
+		with open(os.path.join(BASE_DIR, 'example_data.txt'), 'r') as data_file:
+			self.data = data_file.readlines()
+		self.line = _find_station_line('DEN', self.data)
+		self.winds = _parse_station_line(self.line)
+		self.winds_dict = self.winds.dict()
+
+	def test_3k_winds(self):
+		self.assertEqual(self.winds_dict['winds'][3000]['direction'], 0)
+		self.assertEqual(self.winds_dict['winds'][3000]['speed'], 0)
+
+	def test_6k_winds(self):
+		self.assertEqual(self.winds_dict['winds'][6000]['direction'], 0)
+		self.assertEqual(self.winds_dict['winds'][6000]['speed'], 0)
+
+	def test_9k_winds(self):
+		self.assertEqual(self.winds_dict['winds'][9000]['direction'], 360)
+		self.assertEqual(self.winds_dict['winds'][9000]['speed'], 8)
+
+
+
 class TestParseStationLine(TestCase):
 	def setUp(self):
 		with open(os.path.join(BASE_DIR, 'example_data.txt'), 'r') as data_file:
